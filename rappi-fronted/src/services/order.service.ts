1fcb64782feb9701e1cd3664a4ab1ae18d3a0a46
alejandraemperador <alejandraemperadorg@gmail.com>
2026-03-18 09:00:25 -0500

    9

--- rappi-fronted/src/services/order.service.ts
+++ rappi-fronted/src/services/order.service.ts
@@ -3,7 +3,7 @@ import type { Order, CreateOrderDTO } from "../types/orders.types";
 import type { Store } from "../types/stores.types";
 import type { Product } from "../types/products.types";
 
-const API_URL = "http://localhost:1234/api";
+const API_URL = "https://rappi-lab-backend-alpha.vercel.app/api";
 
 const getAuthHeaders = () => {
     const token = localStorage.getItem('token');
--- rappi-fronted/src/services/store.service.ts
+++ rappi-fronted/src/services/store.service.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:1234/api";
+const API_URL = "https://rappi-lab-backend-alpha.vercel.app/api";
 
 const getAuthHeaders = () => ({
     headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
